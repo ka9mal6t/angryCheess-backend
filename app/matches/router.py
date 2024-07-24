a528f03a7acd4024f0f55515f4d31f8156b7142c
Volodymyr <125738766+ka9mal6t@users.noreply.github.com>
2024-07-24 12:30:07 +0300

    Update router.py

--- app/matches/router.py
+++ app/matches/router.py
@@ -6,6 +6,7 @@ from starlette import status
 from app.users.auth import create_access_token, get_password_hash, verify_password
 from app.users.dao import UsersDAO
 from app.matches.dao import MatchessDAO
+from app.match_history.dao import MatchHistoryDAO
 from app.users.dependencies import get_user
 
 from app.users.models import Users
@@ -22,4 +23,15 @@ async def game_result(current_user: Users = Depends(get_user), match_id: int):
     if not match:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, )
     return match
+    
+
+@router.get("/gameDetails/{match_id}")
+async def gameDetails(current_user: Users = Depends(get_user), match_id: int):
+    match = await MatchesDAO.find_one_or_none(id=match_id, end=True)
+    if not match:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, )
+    match_history = await MatchHistoryDAO.find_all(match_id=match_id)
+    return match_history
+        
+    
     
