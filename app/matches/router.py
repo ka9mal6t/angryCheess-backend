53d789d3f420c62e9051b3bf0e923165635614fa
Volodymyr <125738766+ka9mal6t@users.noreply.github.com>
2024-07-24 12:12:18 +0300

    Update router.py

--- app/matches/router.py
+++ app/matches/router.py
@@ -1,40 +1,25 @@
-# import datetime
-#
-# from fastapi import APIRouter, Response, Depends, Request, HTTPException
-# from starlette import status
-#
-# from app.users.auth import create_access_token, get_password_hash, verify_password
-# from app.users.dao import UsersDAO
-# from app.statistics.dao import StatisticsDAO
-# from app.users.dependencies import get_user
-#
-# from app.users.models import Users
-#
-# router = APIRouter(
-#     prefix="/user",
-#     tags=["Auth & User"]
-# )
-#
-#
-# @router.get("/showMyStatistic",
-#             summary="Get info about auth user",
-#             description="This endpoint for get info about auth user"
-#             )
-# async def info_user(current_user: Users = Depends(get_user)) -> dict:
-#     statistics = await StatisticsDAO.find_one_or_none(user_id=current_user.id)
-#     return {
-#         'id': current_user.id,
-#         'user': {
-#             'id': current_user.id,
-#             'email': current_user.email,
-#             'username': current_user.username,
-#             'inGame': False,
-#             'createdAt': datetime.date(2020, 11, 11),
-#             'roles': ['user', ],
-#         },
-#         'totalGamesPlayed': statistics.games,
-#         'wins': statistics.wins,
-#         'losses': statistics.losses,
-#         'draws': statistics.draws,
-#         'rating': statistics.rating,
-#     }
+import datetime
+
+from fastapi import APIRouter, Response, Depends, Request, HTTPException
+from starlette import status
+
+from app.users.auth import create_access_token, get_password_hash, verify_password
+from app.users.dao import UsersDAO
+from app.statistics.dao import StatisticsDAO
+from app.users.dependencies import get_user
+
+from app.users.models import Users
+
+router = APIRouter(
+    prefix="/matchInfo",
+    tags=["Auth & User"]
+)
+
+
+@router.get("/gameResults/{match_id}")
+async def game_result(current_user: Users = Depends(get_user), match_id: int):
+    match = await MatchesDAO.find_one_or_none(id=match_id)
+    if not match:
+        raise 
+    return match
+    
